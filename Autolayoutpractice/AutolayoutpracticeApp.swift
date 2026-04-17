e281d7f508e67ba540952645200eb54d76ee2e3a
chientzu <j27073021@gmail.com>
2026-04-17 10:06:37 +0800

    Initial Commit

---
+++ Autolayoutpractice/AutolayoutpracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutolayoutpracticeApp.swift
+//  Autolayoutpractice
+//
+//  Created by 孜 on 2026/4/17.
+//
+
+import SwiftUI
+
+@main
+struct AutolayoutpracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
